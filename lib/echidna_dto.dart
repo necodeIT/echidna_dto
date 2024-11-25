6ea4f7721052ef96b39107667814827c904992f4
mcquenji <benjamin.mceachnie@necode.it>
2024-11-25 12:29:26 +0000

    ci: add doc workflow and lining checks

--- lib/echidna_dto.dart
+++ lib/echidna_dto.dart
@@ -1,3 +1,23 @@
+/// # Echidna DTO
+///
+/// This repository contains DTOs (Data Transfer Objects) for the Echidna project. These DTOs are used by the [echidna_webui](https://github.com/necodeIT/echidna_webui) and [echidna_server](https://github.com/necodeIT/echidna_server).
+///
+/// ## Installation
+///
+/// Add this project as a dependency to your `pubspec.yaml` file.
+///
+/// ```yaml
+/// dependencies:
+///   echidna_dto:
+///     git:
+///       url: https://github.com/necodeIT/echidna_dto.git
+/// ```
+///
+/// After that you can import the DTOs in your project.
+///
+/// ```dart
+/// import 'package:echidna_dto/echidna_dto.dart';
+/// ```
 library echidna_dto;
 
 export 'src/client_key.dart';
